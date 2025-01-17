84ceb728b0a5ab898676834fa4504d34045c5314
Siddharth <siddharth122002@gmail.com>
2025-01-17 23:53:21 +0530

    newFuntion for userId

--- app/(root)/events/create/page.tsx
+++ app/(root)/events/create/page.tsx
@@ -1,9 +1,11 @@
 import EventForm from "@/components/shared/EventForm";
 import { getCurrent } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs/server";
 import React from "react";
 
 async function page() {
-  const userId = await getCurrent();
+  const user = await currentUser();
+  const userId = user.publicMetadata.userId as string;
   return (
     <div className=" max-w-7xl p-3 m-auto  mb-8">
       <div className="text-3xl font-bold text-center my-8">Create Event</div>
